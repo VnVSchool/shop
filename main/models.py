79dedf4a66aa5533651dc74ee03650e86ff81c18
Liubomyr.Luzhnyi <l.luzhnuy@gmail.com>
2023-05-10 21:45:38 +0300

    first commit

---
+++ main/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+
+class MenuItem(models.Model):
+    title = models.CharField(max_length=255)
+    link = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.title
